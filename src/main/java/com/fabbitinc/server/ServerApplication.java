b645df39a83147e8663a3a413c4b8d1af160f6ec
moseoh <azqazq195@gmail.com>
2026-03-25 11:11:34 +0900

    chore(server): exclude UserDetailsServiceAutoConfiguration from bootstrap

--- src/main/java/com/fabbitinc/server/ServerApplication.java
+++ src/main/java/com/fabbitinc/server/ServerApplication.java
@@ -2,9 +2,10 @@ package com.fabbitinc.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @ConfigurationPropertiesScan
 public class ServerApplication {
 
